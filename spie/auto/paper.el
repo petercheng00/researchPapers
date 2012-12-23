231490a80116013fbf01ca515f29ff0d5fa1abb7
Peter Cheng <petercheng00@gmail.com>
2012-12-23 11:31:46 -0800

    starting spie stuff

--- spie/auto/paper.el
+++ spie/auto/paper.el
@@ -1,44 +1,23 @@
 (TeX-add-style-hook "paper"
  (lambda ()
     (LaTeX-add-bibliographies
-     "egbib")
+     "report")
     (LaTeX-add-labels
-     "sec:introduction"
-     "sec:simpleTextureMapping"
-     "fig:projection"
-     "sec:directMapping"
-     "fig:scoringFunction"
-     "fig:compareAll"
-     "sec:mappingWithCaching"
-     "sec:existingApproaches"
-     "fig:mosaic3D"
-     "sec:proposedApproach"
-     "fig:pipeline"
-     "sec:projectionAndRotation"
-     "sec:robustSIFTFeatureMatching"
-     "sec:seamMinimization"
-     "fig:projectionAngles"
-     "fig:dagCreation"
-     "sec:blending"
-     "sec:resultsAndConclusions"
-     "fig:results")
+     "sec:intro"
+     "tab:fonts"
+     "sec:title"
+     "sec:sections"
+     "fig:example"
+     "sec:misc"
+     "eq:alpha"
+     "sec:latex")
     (TeX-add-symbols
-     "cvprPaperID"
-     "httilde")
+     '("bm" 1))
     (TeX-run-style-hooks
-     "hyperref"
-     "colorlinks"
-     "subfig"
-     "amssymb"
      "amsmath"
+     "a4paper"
      "graphicx"
-     "epsfig"
-     "times"
-     "cvpr"
      "latex2e"
-     "art10"
-     "article"
-     "10pt"
-     "twocolumn"
-     "letterpaper")))
+     "spie10"
+     "spie")))
 
