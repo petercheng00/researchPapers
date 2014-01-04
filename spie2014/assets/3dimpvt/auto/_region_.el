929dda05a53dc9886fde1212cbc74b2900c290ff
Peter Cheng <petercheng00@gmail.com>
2014-01-03 21:39:30 -0800

    organize stuff

---
+++ spie2014/assets/3dimpvt/auto/_region_.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (LaTeX-add-labels
+     "fig:results")
+    (TeX-add-symbols
+     "cvprPaperID"
+     "httilde")
+    (TeX-run-style-hooks
+     "hyperref"
+     "colorlinks"
+     "subfig"
+     "amssymb"
+     "amsmath"
+     "graphicx"
+     "epsfig"
+     "times"
+     "cvpr"
+     "latex2e"
+     "art10"
+     "article"
+     "10pt"
+     "twocolumn"
+     "letterpaper")))
+
---
+++ spie2014/auto/_region_.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "multirow"
+     "float"
+     "hyperref"
+     "amssymb"
+     "amsmath"
+     "subfig"
+     "graphicx"
+     "latex2e"
+     "spie10"
+     "spie")))
+
